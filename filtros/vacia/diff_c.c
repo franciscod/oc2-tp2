eab5a3611596b2dd68d714da1505533633ce8748
Gabriel <gabriel29m@gmail.com>
2015-09-09 12:32:43 -0300

    Primer intento de diff en c

--- filtros/vacia/diff_c.c
+++ filtros/vacia/diff_c.c
@@ -4,17 +4,27 @@
 #include "../tp2.h"
 
 void diff_c (
-	unsigned char *src,
-	unsigned char *src_2,
-	unsigned char *dst,
-	int m,
-	int n,
-	int src_row_size,
-	int src_2_row_size,
-	int dst_row_size
+	unsigned char *src,         // Primera imagen.
+	unsigned char *src_2,       // Segunda imagen
+	unsigned char *dst,         // Destino
+	int m,                      // Filas
+	int n,                      // Columnas
+	int src_row_size,           // Tamaño de imagen 1
+	int src_2_row_size,         // Tamaño de imagen 2
+	int dst_row_size            // Tamaño destino
 ) {
 	unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
 	unsigned char (*src_2_matrix)[src_2_row_size] = (unsigned char (*)[src_2_row_size]) src_2;
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
 
+    for (i = 0; i < src_row_size; i++){
+        dst_matrix[i] = src_matrix[i] - src_2_matrix[i];
+    }
+    for (i = 0; i < src_row_size; i+=4){
+        int k = max(max(dst_matrix[i], dst_matrix[i+1]), dst_matrix[i+2]);
+        dst_matrix[i] = k;
+        dst_matrix[i+1] = k;
+        dst_matrix[i+2] = k;
+        dst_matrix[i+3] = 255;
+    }
 }
