63f1c014b889ae29c764016b301ededf20cb1646
manumena <manuelmena1993@gmail.com>
2015-09-22 18:52:46 -0300

    aumento pr351c10n

--- filtros/blur_c.c
+++ filtros/blur_c.c
@@ -8,9 +8,8 @@ const int OFFSET_GREEN = 1;
 const int OFFSET_RED = 2;
 const int OFFSET_ALPHA = 3;
 
-float G_sigma(int x, int y, float sigma){
-    // printf("%f %f %f\n", (double) -((double)(x * x + y * y) / (double)(2 * (double)sigma * (double)sigma)), (double) pow(M_E, -((double)(x * x + y * y) / (double)(2 * (double)sigma * (double)sigma))), (double) (2 * M_PI * (double)sigma * (double)sigma));
-    return(pow(M_E, -((x * x + y * y) / (2 * sigma * sigma))) / (2 * M_PI * sigma * sigma));
+double G_sigma(int x, int y, float sigma){
+    return(exp(-((x * x + y * y) / (2 * sigma * sigma))) / (2 * M_PI * sigma * sigma));
 }
 
 void blur_c(unsigned char *src, unsigned char *dst, int cols, int rows, float sigma, int radius){
@@ -18,7 +17,7 @@ void blur_c(unsigned char *src, unsigned char *dst, int cols, int rows, float si
     unsigned char (*dst_matrix)[cols*4] = (unsigned char (*)[cols*4]) dst;
 
     // Creo matriz de convolución
-    float conv_matrix[2 * radius + 1][2 * radius + 1];
+    double conv_matrix[2 * radius + 1][2 * radius + 1];
     for(int i = 0; i < 2 * radius + 1; i++){
         for(int j = 0; j < 2 * radius + 1; j++){
             conv_matrix[i][j] = G_sigma(radius - i, radius - j, sigma);
@@ -30,21 +29,22 @@ void blur_c(unsigned char *src, unsigned char *dst, int cols, int rows, float si
     for(int row = radius; row < rows - radius; row++){
         for(int col = radius; col < cols - radius; col++){
             // Hago la sumatoria
-            float blue = 0;
-            float green = 0;
-            float red = 0;
+            double blue = 0;
+            double green = 0;
+            double red = 0;
             for(int x = -radius; x <= radius; x++){
                 for(int y = -radius; y <= radius; y++){
-                    blue += (float) (src_matrix[row + y][(col + x) * 4 + OFFSET_BLUE]) * conv_matrix[radius - y][radius - x];
-                    green += (float) (src_matrix[row + y][(col + x) * 4 + OFFSET_GREEN]) * conv_matrix[radius - y][radius - x];
-                    red += (float) (src_matrix[row + y][(col + x) * 4 + OFFSET_RED]) * conv_matrix[radius - y][radius - x];
+                    blue += (double) (src_matrix[row + y][(col + x) * 4 + OFFSET_BLUE]) * conv_matrix[radius - y][radius - x];
+                    green += (double) (src_matrix[row + y][(col + x) * 4 + OFFSET_GREEN]) * conv_matrix[radius - y][radius - x];
+                    red += (double) (src_matrix[row + y][(col + x) * 4 + OFFSET_RED]) * conv_matrix[radius - y][radius - x];
                     // printf("%f %f %f\n", (float) (src_matrix[row + y][(col + x) * 4 + OFFSET_GREEN]), conv_matrix[radius - y][radius - x], green);
                 }
             }
 
-            dst_matrix[row][col * 4 + OFFSET_BLUE] = floor(blue);
-            dst_matrix[row][col * 4 + OFFSET_GREEN] = floor(green);
-            dst_matrix[row][col * 4 + OFFSET_RED] = floor(red);
+            // printf("%f %f %f\n", blue, green, red);
+            dst_matrix[row][col * 4 + OFFSET_BLUE] = (unsigned char) blue;
+            dst_matrix[row][col * 4 + OFFSET_GREEN] = (unsigned char) green;
+            dst_matrix[row][col * 4 + OFFSET_RED] = (unsigned char) red;
             dst_matrix[row][col * 4 + OFFSET_ALPHA] = 255;
         }
     }
