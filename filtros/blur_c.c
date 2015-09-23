5ed9c2f2243b8bfe4e8a2d9159fabff84bc9cf9c
gabriel <gabriel29m@gmail.com>
2015-09-23 14:47:50 -0300

    asd

--- filtros/blur_c.c
+++ filtros/blur_c.c
@@ -5,16 +5,13 @@
 #include <math.h>
 #include "../tp2.h"
 
-#define OFFSET i*(2*r+1)+j
-
-void calcular_matriz(double *matriz, int r, float s){
-    int centro = (2*r)/2;
+void calcular_matriz(double **matriz, int r, float s){
     double aux2 = (2*s*s);
     double aux = (double)(1/(M_PI*aux2));
     for(int j = 0; j < 2*r+1; j++){
         for(int i = 0; i < 2*r+1; i++){
-            matriz[j][i] = (double) (aux * exp(-(((j-centro)*(j-centro)+(i-centro)*(i-centro))/aux2)));
-            printf("%.4f ", matriz[j][i]);
+    //        matriz[j][i] = (double) (aux * exp(-(j*j+i*i)/aux2));
+            printf("%.4f ", matriz[0][0]);
         }
         printf("\n");
     }
@@ -24,7 +21,7 @@ void blur_c(unsigned char *src, unsigned char *dst, int cols, int filas, float s
     unsigned char (*src_matrix)[cols*4] = (unsigned char (*)[cols*4]) src;
     unsigned char (*dst_matrix)[cols*4] = (unsigned char (*)[cols*4]) dst;
 
-    double *convol_matrix[2*radius+1][2*radius+1];
+    double convol_matrix[2*radius+1][2*radius+1];
 
     calcular_matriz(convol_matrix, radius, sigma);
 }
