1a7e8b1de24070b98935c8eace7d01aabfe77cc9
Gabriel <gabriel29m@gmail.com>
2015-09-23 13:51:15 -0300

    asd

--- filtros/blur_c.c
+++ filtros/blur_c.c
@@ -7,14 +7,16 @@
 
 #define OFFSET i*(2*r+1)+j
 
-void calcular_matriz(float *matriz, int r, float s){
+void calcular_matriz(double *matriz, int r, float s){
     int centro = (2*r)/2;
     double aux2 = (2*s*s);
     double aux = (double)(1/(M_PI*aux2));
-    for(int j = 0; j < ((2*r)+1); j++){
-        for(int i = 0; i < ((2*r)+1); i++){
-            matriz[OFFSET] = (double) (aux * exp(-(((j-centro)*(j-centro)+(i-centro)*(i-centro))/aux2)));
+    for(int j = 0; j < 2*r+1; j++){
+        for(int i = 0; i < 2*r+1; i++){
+            matriz[j][i] = (double) (aux * exp(-(((j-centro)*(j-centro)+(i-centro)*(i-centro))/aux2)));
+            printf("%.4f ", matriz[j][i]);
         }
+        printf("\n");
     }
 }
 
@@ -22,10 +24,7 @@ void blur_c(unsigned char *src, unsigned char *dst, int cols, int filas, float s
     unsigned char (*src_matrix)[cols*4] = (unsigned char (*)[cols*4]) src;
     unsigned char (*dst_matrix)[cols*4] = (unsigned char (*)[cols*4]) dst;
 
-    // Reservo memoria para la matriz de convolusion.
-    double *convol_matrix = (double *)malloc(((2*radius)+1) * ((2*radius)+1) * sizeof(double));
+    double *convol_matrix[2*radius+1][2*radius+1];
 
     calcular_matriz(convol_matrix, radius, sigma);
-
-    free(convol_matrix);
 }
