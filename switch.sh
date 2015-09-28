32857db94069aa94a703898cc9f21d915f529eb9
Francisco Demartino <demartino.francisco@gmail.com>
2015-09-28 13:38:30 -0300

    espero q esan los ultimos fix

--- switch.sh
+++ switch.sh
@@ -4,24 +4,29 @@
 # ./switch.sh gabriel-orig
 #
 
+BASE_PATH=./filtros/
+
+VACIA_PATH=${BASE_PATH}vacia
+
 SWITCH_DIR=$1 # primer parametro
+SW_PATH=${BASE_PATH}${SWITCH_DIR}
 
 # primero que nada limpia el build dir
 make clean
 
 for f in blur_c.c blur_asm.asm diff_c.c diff_asm.asm; do
 	# limpia lo que habia
-	rm filtros/$f
+	rm -f $BASE_PATH/$f
 
 	# si no esta el archivo este
-	if [ ! -f ./filtros/$SWITCH_DIR/$f ]; then
+	if [ ! -f $SW_PATH/$f ]; then
 		# pone la implementacion vacia
 		# ln -s vacia/$f filtros/$f
-		cp filtros/vacia/$f filtros/$f
+		cp $VACIA_PATH/$f $BASE_PATH/$f
 	else
 		# si esta implementado en SWITCH_DIR, lo pone
 		# ln -s $SWITCH_DIR/$f filtros/$f
-		cp filtros/$SWITCH_DIR/$f filtros/$f
+		cp $SW_PATH/$f $BASE_PATH/$f
 	fi
 done
 
