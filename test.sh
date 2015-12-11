4c8999140e6000c85d5ae70eb6aeb10eeff31dfb
Francisco Demartino <demartino.francisco@gmail.com>
2015-12-10 21:39:30 -0300

    agregar ./switch a ./test

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
-$VAL build/tp2 -i asm blur img/circulos.bmp 5 15
-$VAL build/tp2 -i c blur img/circulos.bmp 5 15
+./switch.sh comun
+build/tp2 -i asm blur img/circulos.bmp 5 15
+build/tp2 -i c blur img/circulos.bmp 5 15
 
